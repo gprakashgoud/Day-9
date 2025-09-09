f127e6fe859c3b79a3de7b625f5d0d9380a511cf
gprakashgoud <gprakashgoud611@gmail.com>
2025-09-09 21:52:24 +0530

    Control Statements

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+package com.codegnan.loopingstatements;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num=scanner.nextInt();
+        System.out.println("fibonacci series");
+  for(int i=0; i<=1; i++);
+	  if(num<=1);{
+		  System.out.println(num);
+	  }
+	  if(num>1) {
+	    System.out.println((num-1)+(num-2));
+		  }
+	  scanner.close();
+  }
+	}
+
